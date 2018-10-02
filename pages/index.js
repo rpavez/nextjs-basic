56da3dd2eb56dfcc6359804b47fd5d4a7b75d343
Rodrigo Pavez <rpavez@icracked.com>
2018-10-02 11:59:53 -0700

    first commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import App from '../components/App'
+import AppContainer from '../content/AppContainer'
+// import Header from '../components/Header'
+// import Submit from '../components/Submit'
+// import PostList from '../components/PostList'
+
+// import Header from "components/Header/Header.jsx";
+// import Footer from "components/Footer/Footer.jsx";
+// import Sidebar from "components/Sidebar/Sidebar.jsx";
+
+export default () => (
+  <AppContainer></AppContainer>
+)
